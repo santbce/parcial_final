16faed69688b09c7077898db804c0e487c61c1dd
santbce <santiago_jos.becerra@uao.edu.co>
2025-05-24 18:45:57 -0500

    Subiendo contenido del archivo

---
+++ interfaz.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+from tkinter import ttk, messagebox
+import gestion 
+
+
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
+
